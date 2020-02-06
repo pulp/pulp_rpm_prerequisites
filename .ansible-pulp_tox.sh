14c64863c87432edd507e1f396f644231a207340
Mike DePaulo <mikedep333@redhat.com>
2020-02-06 17:22:31 -0500

    Problem: pulp_use_system_wide_pkgs is often overlooked
    during pulp_rpm install.
    
    Solution: Let's set it in the pulp_rpm_prerequisites role via
    include_vars on the vars/main.yml
    
    fixes: #5992
    https://pulp.plan.io/issues/5992
    ansible-pulp rpm plugin: Failed building wheel for PyGObject Failed building wheel for pycairo

--- .ansible-pulp_tox.sh
+++ .ansible-pulp_tox.sh
@@ -9,8 +9,6 @@ if [ ! -e roles/pulp.pulp_rpm_prerequisites ]; then
   ln -s $TRAVIS_BUILD_DIR roles/pulp.pulp_rpm_prerequisites
 fi
 
-sed -i 's/pulp_use_system_wide_pkgs: false/pulp_use_system_wide_pkgs: true/g' roles/pulp/defaults/main.yml
-find ./molecule/*/group_vars/all -exec sh -c "yq w -i {} pulp_use_system_wide_pkgs true" \;
 find ./molecule/*source*/group_vars/all -exec sh -c "yq w -i {} pulp_install_plugins.pulp-rpm.source_dir \/var\/lib\/pulp\/devel\/pulp_rpm" \;
 find ./molecule/*upgrade*/group_vars/all -exec sh -c "yq w -i {} pulp_install_plugins.pulp-rpm.upgrade true" \;
 find ./molecule/*/group_vars/all -exec sh -c "yq w -i {} pulp_install_plugins.pulp-rpm.prereq_role pulp.pulp_rpm_prerequisites" \;
