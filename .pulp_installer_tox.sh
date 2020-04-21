ccb45aa1c27cd8e054a6f35cd375e17ad6adc39c
Mike DePaulo <mikedep333@redhat.com>
2020-04-21 13:50:37 -0400

    Separate the pulp_installer CI tests: release vs source
    Required PR: https://github.com/pulp/pulp_installer/pull/269
    
    re: #6535
    
    [noissue]

--- .pulp_installer_tox.sh
+++ .pulp_installer_tox.sh
@@ -13,7 +13,7 @@ fi
 find ./molecule/*/group_vars/all -exec sh -c "yq w -i {} pulp_version 3.2.1" \;
 #find ./molecule/*/group_vars/all -exec sh -c "yq w -i {} pulp_git_commitish 3.3.0" \;
 find ./molecule/*/group_vars/all -exec sh -c "yq w -i {} pulp_install_plugins.pulp-file.version 0.2.0 " \;
-find ./molecule/default-upgrade/group_vars/all -exec sh -c "yq d -i {} pulp_install_plugins.pulp-file.upgrade" \;
+find ./molecule/release-upgrade/group_vars/all -exec sh -c "yq d -i {} pulp_install_plugins.pulp-file.upgrade" \;
 #find ./molecule/*/group_vars/all -exec sh -c "yq w -i {} pulp_install_plugins.pulp-file.git_commitish 0.2.0 " \;
 #find ./molecule/*/group_vars/all -exec sh -c "yq d -i {} pulp_install_plugins.pulp-file" \;
 
@@ -26,8 +26,8 @@ find ./molecule/*/group_vars/all -exec sh -c "echo; echo {}; cat {}" \;
 find ./molecule/*upgrade*/molecule.yml -exec sed -i '/quay.io\/pulp\/pulp-ci-dbuster:3.0.0/,+3 d' {} \;
 find ./molecule/*upgrade*/molecule.yml -exec sed -i '/debian-10/d' {} \;
 find ./molecule/*/molecule.yml -exec sed -i '/debian-10/,+3 d' {} \;
-find ./molecule/*-upgrade/molecule.yml -exec sed -i 's/pulp-ci-c7:3.0.0/pulp_rpm-ci-c7:3.1.0/g' {} \;
-find ./molecule/*-upgrade/molecule.yml -exec sed -i 's/pulp-ci-f31:3.0.0/pulp_rpm-ci-f31:3.1.0/g' {} \;
+find ./molecule/*upgrade*/molecule.yml -exec sed -i 's/pulp-ci-c7:3.0.0/pulp_rpm-ci-c7:3.1.0/g' {} \;
+find ./molecule/*upgrade*/molecule.yml -exec sed -i 's/pulp-ci-f31:3.0.0/pulp_rpm-ci-f31:3.1.0/g' {} \;
 
 tox && exit 0 || true
 sleep 45
